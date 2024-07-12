d24f534a852f2dab5dafa371060c50fade87fa62
Jbduret <jb.duret@hotmail.fr>
2024-07-12 11:54:45 +0200

    Modify header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { FlexBox, Alignment } from '@lumx/react';
+import { Thumbnail, Size } from '@lumx/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import Search from '../Search';
 
 const Header = ({ setSearchValue }) => (
 	<header className="lumx-spacing-padding-big header">
-		<FlexBox vAlign={Alignment.right}>
-			<Search onSearch={setSearchValue} />
-		</FlexBox>
+		<Thumbnail className="photo" image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png" size={Size.xl} alt="Marvel cover" />
+		<Search onSearch={setSearchValue} />
 	</header>
 );
 
